278824a6f1c5336cd927118398311bb3d8375464
Oirien <robert.mayo.ch@gmail.com>
2023-09-19 17:31:31 +0100

    initial commit

---
+++ src/main/java/com/codebase/Borrower.java
@@ -0,0 +1,25 @@
+package com.codebase;
+
+import java.util.ArrayList;
+
+public class Borrower {
+
+    private String name;
+    private ArrayList<Book> borrowedBooks;
+
+    public Borrower(String name) {
+        this.name = name;
+        this.borrowedBooks = new ArrayList<>();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int borrowAmount(){
+        return this.borrowedBooks.size();
+    }
+    public void addBook(Book book){
+        this.borrowedBooks.add(book);
+    }
+}
---
+++ src/main/java/com/codebase/Library.java
@@ -0,0 +1,54 @@
+package com.codebase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Library {
+    private int shelfSpace;
+    private ArrayList<Book> bookCollection;
+    private HashMap<String, Integer> genres;
+
+    public Library(int shelfSpace) {
+        this.shelfSpace = shelfSpace;
+        this.bookCollection = new ArrayList<>();
+        this.genres = new HashMap<>();
+    }
+
+    public boolean hasCapacity(){
+        return shelfSpace > bookCollection.size();
+    }
+    public int collectionSize(){
+        return bookCollection.size();
+    }
+    public void addBookToCollection(Book book){
+        if (hasCapacity()){
+            bookCollection.add(book);
+        }
+    }
+    public boolean bookInStock(Book book){
+        if(bookCollection.contains(book)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public void lendBookToBorrower(Book book, Borrower borrower){
+        bookCollection.remove(book);
+        borrower.addBook(book);
+    }
+    public void genreAdd(Book book){
+        String genre = book.getGenre();
+        if(genres.containsKey(genre)){
+            int genreCount = genres.get(genre);
+            genreCount++;
+            genres.put(genre, genreCount);
+        } else {
+            genres.put(genre, 1);
+        }
+    }
+
+    public int genreCount(String genre){
+        return genres.get(genre);
+    }
+
+}
